21e6148fde030792612c118d9a8c3658916ee028
Zura014 <zuragagnidze50@gmail.com>
2023-09-30 20:19:22 +0400

    980

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from './../../models/Todo';
+import { JsonPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+  todos: Todo[] = [];
+
+  inputTodo:string = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.todos = [
+    ];
+  };
+
+  toggleDone (id: number) {
+    this.todos.map((j, i) => {
+      if( i == id) j.completed = !j.completed;
+      
+      return j;
+    })
+  };
+
+  deleteTodos(id: number){
+    this.todos = this.todos.filter((v, i) => i !== id);
+  };
+
+  addTodos(){
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false,
+    });
+    this.inputTodo = "";
+    localStorage.setItem(JSON.stringify(this.todos), "todos");
+  };
+  
+};
